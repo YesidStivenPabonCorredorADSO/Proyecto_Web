86431499f9c080f11cf8939b453c3b8088ee18ef
Yesid Pabon <yesidpabon145@gmail.com>
2024-07-25 12:41:42 -0500

    hpta ban colombia

---
+++ App_javascript/App.js
@@ -0,0 +1,9 @@
+// Llamado al Doom
+
+// El apartado de login
+const $input_correo=document.getElementById("correo");
+const $input_contraseña=document.getElementById("contraseña");
+
+// El apartado de registro
+const $inpot_nombre=document.getElementById();
+const $inpot_apellido=document.getElementById();
