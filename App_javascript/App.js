0fa3cd1cd7162591375e819464ced2c65bf9f46b
Yesid Pabon <yesidpabon145@gmail.com>
2024-07-26 11:02:48 -0500

    js

--- App_javascript/App.js
+++ App_javascript/App.js
@@ -5,5 +5,18 @@ const $input_correo=document.getElementById("correo");
 const $input_contraseña=document.getElementById("contraseña");
 
 // El apartado de registro
-const $inpot_nombre=document.getElementById();
-const $inpot_apellido=document.getElementById();
+const $input_nombre=document.getElementById("name");
+const $input_apellido=document.getElementById("name_apellido");
+
+// El apartado de informacion basica
+const $chebox=document.getElementById("radiobutton1");
+const $input_fecha=document.getElementById("fecha");
+
+// El apartado de crear contraseña
+const $input_crear_contrase=document.getElementById("crear_contraseña");
+const $input_confirmar_contraseña=document.getElementById("confirmar_contraseña");
+
+// El apartado de ingresar correo 
+
+const $input_agregar_correo=document.getElementById("agregar_correo");
+const $input_confirmar_correo=document.getElementById("confirmar_correo");
